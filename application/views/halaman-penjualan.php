ddb6e8ddff0c8fc86a4c6dee17645e969d87aef7
dazelpro <dazelpro@gmail.com>
2020-08-19 10:57:55 +0700

    paginations

--- application/views/halaman-penjualan.php
+++ application/views/halaman-penjualan.php
@@ -5,8 +5,39 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Halaman Penjualan</title>
 	<link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/page.css');?>" rel="stylesheet">
-
+	<style>
+		.simple-pagination ul {
+			margin: 0 0 20px;
+			padding: 10px 10px 10px 10px;
+			list-style: none;
+			text-align: center
+		}
+		.simple-pagination li {
+			display: inline-block;
+			margin-right: 5px
+		}
+		.simple-pagination li a,
+		.simple-pagination li span {
+			color: #666;
+			padding: 5px 15px 5px 15px;
+			text-decoration: none;
+			border: 1px solid #eee;
+			background-color: #fff;
+			box-shadow: 0 0 10px 0 #eee
+		}
+		.simple-pagination .current {
+			color: #fff;
+			background-color: #0069d9;
+			border-color: #0069d9;
+		}
+		.simple-pagination .next.current,
+		.simple-pagination .prev.current {
+			color: #666;
+			background: #fff;
+			border: 1px solid #eee;
+			cursor: not-allowed
+		}
+	</style>
 </head>
 <body>
 	<div class="container">
@@ -31,6 +62,7 @@
 			</table>
 		</form>
 		<br>
+		<div id="pagination-container"></div>
 		<table class="table">
 			<thead>
 				<tr>
@@ -60,5 +92,24 @@
 			</tbody>
 		</table>
 	</div>
+	<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/jquery.simplePagination.js');?>"></script>
+	<script>
+		var items = $(".list-wrapper .list-item");
+		var numItems = items.length;
+		var perPage = 5;
+		items.slice(perPage).hide();
+		$('#pagination-container').pagination({
+			items: numItems,
+			itemsOnPage: perPage,
+			prevText: "Previous",
+			nextText: "Next",
+			onPageClick: function (pageNumber) {
+				var showFrom = perPage * (pageNumber - 1);
+				var showTo = showFrom + perPage;
+				items.hide().slice(showFrom, showTo).show();
+			}
+		});
+	</script>
 </body>
 </html>
